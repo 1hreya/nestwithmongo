9dd626c0047568e36b1f8ec4fc1b41d62e17c204
1hreyan <shreyad.brainerhub@gmail.com>
2024-04-18 15:12:55 +0530

    new code

---
+++ src/models/user.schema.ts
@@ -0,0 +1,24 @@
+import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+const { Schema } = mongoose;
+
+export const UserSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  role: { type: String, required: true },
+});
+
+UserSchema.pre('save', async function (next: any) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const hashed = await bcrypt.hash(this['password'], 10);
+    this['password'] = hashed;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
---
+++ src/user/DTO/update-user-dto.ts
@@ -0,0 +1,10 @@
+export interface UpdateUserDto {
+    email: string;
+    name: string;
+    role: string;
+  }
+  
+  export interface DeleteUserDto {
+    email: string;
+  }
+  
